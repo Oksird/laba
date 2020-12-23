3bd056fd91455243aaa14321ddca3b86d833cd28
Maxim Muzychenko <m.muz.al3105@gmail.com>
2020-12-23 17:44:09 +0200

    Change commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,8 +16,8 @@ public class DemoApplication {
 	}
 
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	public String hello(@RequestParam(value = "q", defaultValue = "World") String q) {
+		return String.format("Hello %s!", q);
 	}
 
 }
